4c43ca585e5b3e99a24b3c448b745fe851efffd0
Samuel Yossef <34128343+samuelyossef@users.noreply.github.com>
2024-04-15 18:59:40 -0300

    Update llm_api.py

--- llm_api.py
+++ llm_api.py
@@ -1,12 +1,13 @@
-
-import os
+import streamlit as st
 from dotenv import load_dotenv
+from minha_script import clear_convo, init
 from typing import List, Dict
 from openai import AsyncOpenAI
 
-# Environment variables
-_ = load_dotenv('.env')
+# Carregar variáveis de ambiente
+load_dotenv('.env')
 
+# Configurações do modelo
 model_config = {
     "model": "gpt-3.5-turbo",
     "temperature": 0.7,
@@ -16,9 +17,40 @@ model_config = {
     "presence_penalty": 0,
 }
 
+# Função para integração com a OpenAI
 async def openai_chatbot_chain(messages: List[Dict[str, str]], settings: dict = model_config):
     client = AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
     stream_response = await client.chat.completions.create(
         messages=messages, stream=True, **settings
     )
     return stream_response
+
+# Inicialização da interface do usuário
+init()
+
+# Componentes do Streamlit
+st.title("Agente RAG :robot_face:")
+api_key = st.sidebar.text_input("Chave da API OpenAI", type="password")
+st.sidebar.markdown("""Este aplicativo demonstra o Agente RAG. É capaz de rotear a consulta do usuário para a escolha apropriada 
+de resumir um documento, fornecer informações adicionais de um banco de dados vetorial ou fornecer uma simples resposta de acompanhamento.
+O agente em si não depende de nenhum orquestrador (por exemplo: llama-index, langchain, etc.) e usa apenas haystack-ai para indexar e recuperar documentos.""")
+
+# Atualizar a chave da API OpenAI
+openai.api_key = api_key
+
+# Botão para limpar a conversa
+clear_button = st.sidebar.button("Limpar Conversa", key="clear", on_click=clear_convo)
+
+# Entrada do usuário
+user_input = st.text_input("Diga algo")
+
+# Lógica de conversa
+if user_input:
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    res = await openai_chatbot_chain([{"role": "user", "content": user_input}])
+    st.session_state.messages.append({"role": "assistant", "content": res["choices"][0]["message"]["content"]})
+
+# Exibir mensagens
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
